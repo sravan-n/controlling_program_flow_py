314ef6320453af9e2dfebe832179baa423a285ce
Sravan Kumar Nuthalapati <skn.sravan@gmail.com>
2025-11-25 23:50:31 -0600

    test cases updated

--- module_2/exercise_4/test.py
+++ module_2/exercise_4/test.py
@@ -16,17 +16,36 @@ def test_iso_8601():
     
     # Put your test cases here
 
-    result = func.iso_8601('time')
-    introcs.assert_equals(False, result)
+    result = func.iso_8601('01:01:01')
+    introcs.assert_equals(True, result)
 
     result = func.iso_8601('1:1:1')
     introcs.assert_equals(False, result)
 
-    result = func.iso_8601('01:01:01')
-    introcs.assert_equals(True, result)
-
-    result = func.iso_8601('26:83:100')
+    result = func.iso_8601('26:26:26')
     introcs.assert_equals(False, result)
+
+    result = func.iso_8601('time')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('01:01')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('01:01:01:01')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('aa:aa:aa')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('111')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('11:60:00')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('11:59:60')
+    introcs.assert_equals(False, result)
+
 
 
 if __name__ == '__main__':
