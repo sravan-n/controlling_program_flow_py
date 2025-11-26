6c0a719b2e2140efdb06521c80d7188706c8ca25
Sravan Kumar Nuthalapati <skn.sravan@gmail.com>
2025-11-25 23:17:13 -0600

    2.4

---
+++ module_2/exercise_4/test.py
@@ -0,0 +1,34 @@
+"""  
+A test script for the function iso_8601.
+
+Author: Sravan Kumar Nuthalapati
+Date: 11/25/2025
+"""
+import func
+import introcs
+
+
+def test_iso_8601():
+    """
+    Test procedure for the function iso_8601()
+    """
+    print('Testing iso_8601()')
+    
+    # Put your test cases here
+
+    result = func.iso_8601('time')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('1:1:1')
+    introcs.assert_equals(False, result)
+
+    result = func.iso_8601('01:01:01')
+    introcs.assert_equals(True, result)
+
+    result = func.iso_8601('26:83:100')
+    introcs.assert_equals(False, result)
+
+
+if __name__ == '__main__':
+    test_iso_8601()
+    print('Module func passed all tests.')
